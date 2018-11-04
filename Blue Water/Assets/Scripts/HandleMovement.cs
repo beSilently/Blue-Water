366dd390822d18e475fc00360aa761483c4791b9
Anna G <anngoncharova08@gmail.com>
2018-11-04 20:21:41 +0300

    Добавлено управление handle-ом
    Handle (игрок) следует за мышью, когда зажата левая клавиша. Возможен выход за пределы экрана.

--- Blue Water/Assets/Scripts/HandleMovement.cs
+++ Blue Water/Assets/Scripts/HandleMovement.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandleMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+public class HandleMovement : MonoBehaviour
+{
+
+    public GameObject player;
+
+    bool dragging = false;
+    Vector3 mouseStartPos;
+    Vector3 playerStartPos;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragging = true;
+            mouseStartPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            playerStartPos = player.transform.position;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+        }
+        else 
+        {
+            player.transform.position += new Vector3(0, Camera.main.GetComponent<CameraMovement>().cameraSpeed, 0);
+        }
+
+        if (dragging)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            Vector3 move = mousePos - mouseStartPos;
+            player.transform.position = playerStartPos + move;
+        }
+    }
 }
