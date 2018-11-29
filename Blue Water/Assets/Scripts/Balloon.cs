131f4eb2affc81c61d26025159a48354a9f4e124
Anna G <anngoncharova08@gmail.com>
2018-11-29 23:44:12 +0300

    Генерация уровней
    1. Добавлена генерация уровней.
    2. Добавлен счетчик уровней (скрипт).
    3. Полупрозрачный номер уровня появляется поверх фона.
    4. Уничтожение объектов после пересечения LevelEnd.

--- Blue Water/Assets/Scripts/Balloon.cs
+++ Blue Water/Assets/Scripts/Balloon.cs
@@ -7,10 +7,15 @@ using UnityEngine.UI;
 public class Balloon : MonoBehaviour {
 
 	private Text ScoreText;
+    private Text LevelText;
+    int currentLevelNumber;
 
-	private void Start () {
+    private void Start () {
 		ScoreText = GameManager.gm.GameplayUI.Find ("ScorePlaceholder").Find ("Score").GetComponent<Text> ();
-	}
+        LevelText = GameManager.gm.GameplayUI.Find("LevelPlaceholder").Find("Level").GetComponent<Text>();
+        currentLevelNumber = 1;
+        LevelText.text = currentLevelNumber.ToString() + "/99";
+    }
 	
 	private void Update () {
 		ScoreText.text = Mathf.Max (0, Mathf.FloorToInt (transform.position.y)).ToString ();
@@ -20,11 +25,12 @@ public class Balloon : MonoBehaviour {
 		if (other.tag == "Obstacle") {
 			print ("GAME OVER!");
 			SceneManager.LoadScene ("Gameplay");
-;		}
+		}
 		else if (other.tag == "LevelEnd") {
 			other.tag = "Untagged"; //Can trigger only once (needs, bcz balloon has 2 colliders)
-			GameManager.gm.lm.SpawnLevel ();
-		}
-	}
+            currentLevelNumber++;
+            LevelText.text = currentLevelNumber.ToString() + "/99";
+        }
+    }
 
 }
---
+++ Blue Water/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public GameObject[] availableLevels;
+    public List<GameObject> currentLevels;
+    private float screenHeightInPoints;
+    int currentLevelNumber;
+
+    private void Start()
+    {
+        currentLevelNumber = 2;
+        screenHeightInPoints = 2.0f * Camera.main.orthographicSize;
+        StartCoroutine(GeneratorCheck());
+    }
+
+    void AddLevel(float farthestLevelEndY)
+    {
+        int randomLevelIndex = Random.Range(0, availableLevels.Length);
+        GameObject level = (GameObject)Instantiate(availableLevels[randomLevelIndex]);
+        float levelHeight = level.transform.Find("Background").localScale.y;
+        float levelCenter = farthestLevelEndY + levelHeight * 0.5f;
+        level.transform.position = new Vector3(0, levelCenter, 0);
+        level.transform.Find("LevelUI").Find("LevelNumber").GetComponent<Text>().text = currentLevelNumber.ToString();
+        currentLevelNumber++;
+        currentLevels.Add(level);
+    }
+    private void GenerateLevelIfRequired()
+    {
+        List<GameObject> levelsToRemove = new List<GameObject>();
+        bool addLevels = true;
+        float playerY = transform.position.y;
+        float removeLevelY = playerY - (screenHeightInPoints * 2.0f);
+        float addLevelY = playerY + screenHeightInPoints;
+        float farthestLevelEndY = 0;
+        foreach (var level in currentLevels)
+        {
+            float levelHeight = level.transform.Find("Background").localScale.y;
+            float levelStartY = level.transform.position.y - (levelHeight * 0.5f);
+            float levelEndY = levelStartY + levelHeight;
+            if (levelStartY > addLevelY)
+            {
+                addLevels = false;
+            }
+            if (levelEndY < removeLevelY)
+            {
+                levelsToRemove.Add(level);
+            }
+            farthestLevelEndY = Mathf.Max(farthestLevelEndY, levelEndY);
+        }
+        foreach (var room in levelsToRemove)
+        {
+            currentLevels.Remove(room);
+            Destroy(room);
+        }
+        if (addLevels)
+        {
+            AddLevel(farthestLevelEndY);
+        }
+    }
+
+    private IEnumerator GeneratorCheck()
+    {
+        while (true)
+        {
+            GenerateLevelIfRequired();
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+}
